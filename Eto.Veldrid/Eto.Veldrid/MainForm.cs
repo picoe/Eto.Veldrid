8e45059d5a5aeb7d29e3d2ed3112ab68c540c0bf
Rob Martens <robert.martens@gmail.com>
2018-12-17 07:15:43 -0500

    Add project files.

---
+++ Eto.Veldrid/Eto.Veldrid/MainForm.cs
@@ -0,0 +1,14 @@
+using System;
+using Eto.Forms;
+using Eto.Drawing;
+
+namespace Eto.Veldrid
+{
+	public partial class MainForm : Form
+	{
+		public MainForm()
+		{
+			InitializeComponent();
+		}
+	}
+}
