39f0d781729477d111609f31e02733564295d9c4
Rob Martens <robert.martens@gmail.com>
2019-02-25 00:44:43 -0500

    Don't set up Veldrid until form is ready
    Direct3D 11 is temperamental, and Vulkan doesn't want to work at all, until both the VeldridSurface is ready and the MainForm has been shown. Divas.

--- Eto.Veldrid/Eto.Veldrid/MainForm.cs
+++ Eto.Veldrid/Eto.Veldrid/MainForm.cs
@@ -9,9 +9,35 @@ namespace Eto.VeldridSurface
 	{
 		public VeldridDriver Driver { get; } = new VeldridDriver();
 
+		private bool _veldridReady = false;
+		public bool VeldridReady
+		{
+			get { return _veldridReady; }
+			private set
+			{
+				_veldridReady = value;
+
+				SetUpVeldrid(FormReady, VeldridReady);
+			}
+		}
+
+		private bool _formReady = false;
+		public bool FormReady
+		{
+			get { return _formReady; }
+			set
+			{
+				_formReady = value;
+
+				SetUpVeldrid(FormReady, VeldridReady);
+			}
+		}
+
 		public MainForm(Action<VeldridSurface, GraphicsBackend> initOther, GraphicsBackend backend)
 		{
 			InitializeComponent();
+
+			Shown += (sender, e) => FormReady = true;
 
 			var surface = new VeldridSurface(initOther, backend);
 
@@ -33,21 +59,26 @@ namespace Eto.VeldridSurface
 
 			Driver.Surface = surface;
 
-			surface.VeldridSurfaceInitialized += Surface_VeldridSurfaceInitialized;
+			surface.VeldridSurfaceInitialized += (sender, e) => VeldridReady = true;
 		}
 
-		private void Surface_SizeChanged(object sender, EventArgs e)
+		private void SetUpVeldrid(bool formReady, bool veldridReady)
 		{
+			if (!(formReady && VeldridReady))
+			{
+				return;
+			}
+
+			Driver.SetUpVeldrid();
+
 			VeldridSurface s = Driver.Surface;
 			s?.Swapchain?.Resize((uint)s.Width, (uint)s.Height);
 
 			Driver.Draw();
 		}
 
-		private void Surface_VeldridSurfaceInitialized(object sender, EventArgs e)
+		private void Surface_SizeChanged(object sender, EventArgs e)
 		{
-			Driver.SetUpVeldrid();
-
 			VeldridSurface s = Driver.Surface;
 			s?.Swapchain?.Resize((uint)s.Width, (uint)s.Height);
 
